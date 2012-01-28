206cbfc1742c7cae1003f7a38f27f71f282a3034
Chris Kowalik <chris@nu7hat.ch>
2012-01-28 03:59:54 -0200

    scripts: fixed msg.sh

--- scripts/msg.sh
+++ scripts/msg.sh
@@ -4,8 +4,10 @@ osname=`uname -s`
 color=36
 
 if [ "$osname" != "Darwin" ]; then
-    alias echo echo -e
+	echo="echo -e"
+else
+	echo=echo
 fi
 
 text=$(echo $@ | sed -e 's/\*\([^\*]*\)\*/\\033['$color';1m\1\\033[0m/g')
-echo "\n\033[35m--- \033[($color)m$text\033[0m"
+$echo "\n\033[35m--- \033["$color"m$text\033[0m"
